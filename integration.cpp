06e82b51b4a8f7bb5ca73abd687d54c1e9b38add
dmp942 <dpederson@utexas.edu>
2014-10-07 15:04:04 -0500

    swapped the integration .cpp and .hpp because they were mixed

--- integration.cpp
+++ integration.cpp
@@ -1,14 +1,33 @@
-#ifndef _INTEGRATION_H
-#define _INTEGRATION_H
 
-#include <math.h>
-#include <stdlib.h>
-#include <iostream>
+#include "integration.hpp"
 
-double integrate_trapezoid(double (*function_handle)(double), double x_start, double x_end, double h);
+double integrate_trapezoid(double (*function_handle)(double), double x_start, double x_end, double h){
+	// declare vars
+	double value = 0.0;
+	unsigned int nsteps = (unsigned int) ((x_end - x_start)/h);
+	
+	// add up values
+	for (unsigned int i=1; i<nsteps; i++){
+		value += h*function_handle(x_start + i*h);
+	}
+	value += h/2*function_handle(x_start);
+	value += h/2*function_handle(x_end);
+	
+	return value;
+}
 
-double integrate_simpson(double (*function_handle)(double), double x_start, double x_end, double h);
+double integrate_simpson(double (*function_handle)(double), double x_start, double x_end, double h){
+	// declare vars
+	double value = 0.0;
+        unsigned int ninter = (unsigned int) ((x_end - x_start)/h);
+	unsigned int nsteps = ninter + 1;	
 
+	// add up values
+	for (unsigned int i=1; i<(nsteps+1)/2; i++){
+                value += h/3*4*function_handle(x_start + (2*i-1)*h) + h/3*2*function_handle(x_start + 2*i*h);
+        }
+	value += h/3*function_handle(x_start);
+	value += h/3*function_handle(x_end);
 
-
-#endif
+	return value;
+}
