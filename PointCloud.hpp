78991f537577a26418782c277488185941092c0b
dmp942 <dpederson@utexas.edu>
2014-10-24 10:54:29 -0500

    added visualizer and pointcloud placeholders

---
+++ PointCloud.hpp
@@ -0,0 +1,23 @@
+#ifndef _POINTCLOUD_H
+#define _POINTCLOUD_H
+
+// this is a placeholder for the PointCloud class
+
+// it should hold the following elements (at least):
+//  - x,y,z,intensity
+//  - return number
+//  - classification
+//  - extra fields, extra field names
+//  - projection (UTM, ECEF, lat/lon) (choose one to always work in... I'm thinking ECEF)
+//
+// and it should have the following functions (at least):
+//  - read LAS file
+//  - write LAS file
+//  - read ascii delimited x,y,z,i,c
+//  - write ascii delimited x,y,z,i,c
+//  - print a summary of data
+//  - recalculate the x,y,z extents
+//  - conversion to/from ECEF/latlon/UTM
+//  - create/destroy data vectors
+
+#endif
---
+++ visualixer.hpp
@@ -0,0 +1,11 @@
+#ifndef _VISUALIXER_H
+#define _VISUALIXER_H
+
+// this contains definitions for the openGL visualizer widget
+// basically just a placeholder and reminder for now,
+// but the visualizer should be able to:
+//  - visualize flow in 2d and 3d and move around in it
+//  - visualize the geometry in 2d and 3d and move around in it
+//  - visualize and interact with point clouds
+
+#endif
