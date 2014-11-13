f4c07b832c6f26a1c72330fcabd5a16906a87f73
pederson <dpederson@utexas.edu>
2014-11-12 23:26:24 -0600

    did some work on the mesh class and creation of a regular grid. not working at the moment

---
+++ mesh_class.cpp
@@ -0,0 +1,196 @@
+#include "mesh_class.hpp"
+
+using namespace std;
+
+#define _TEST_
+
+Node::Node(){
+
+}
+
+Node::~Node(){
+  
+}
+
+//******************************************************************************************************
+
+Mesh::Mesh(){
+  x_offset = 0;
+  y_offset = 0;
+  z_offset = 0;
+
+  xmin = 0;
+  xmax = 0;
+  ymin = 0;
+  ymax = 0;
+  zmin = 0;
+  zmax = 0;
+}
+
+Mesh::~Mesh(){
+  if (mesh_nodes.size() > 0){
+    for (unsigned int i=0; i<mesh_nodes.size(); i++) delete mesh_nodes[i];
+  }
+}
+
+void Mesh::print_summary(){
+  cout << "Mesh Summary: " << endl;
+  if (mesh_nodes.size() == 0){
+    cout << "  Mesh is empty!" << endl;
+    return;
+  }
+
+  cout << "  type: ";
+  if (mesh_type==REGULAR) cout << "REGULAR" << endl;
+  else if (mesh_type==UNSTRUCTURED_TRI) cout << "UNSTRUCTURED TRI" << endl;
+  else if (mesh_type==UNSTRUCTURED_QUAD) cout << "UNSTRUCTURED QUAD" << endl;
+  else cout << "UNKNOWN" << endl;
+  cout << "  num_dims: " << num_dims << endl;
+  cout << "  num_nodes: " << mesh_nodes.size() << endl;
+  //cout << "  x extents: [" << xmin << ", " << xmax << "]" << endl;
+  //cout << "  y extents: [" << ymin << ", " << ymax << "]" << endl;
+  //cout << "  z extents: [" << zmin << ", " << zmax << "]" << endl;
+
+
+}
+
+MeshType Mesh::get_mesh_type(){
+  return mesh_type;
+}
+
+void Mesh::set_mesh_type(MeshType type){
+  mesh_type = type;
+  return;
+}
+
+unsigned int Mesh::get_num_dims(){
+  return num_dims;
+}
+
+void Mesh::set_num_dims(unsigned int ndims){
+  num_dims = ndims;
+  return;
+}
+
+unsigned int Mesh::get_num_nodes(){
+  return mesh_nodes.size();
+}
+
+void Mesh::set_num_nodes(unsigned int number_of_nodes){
+  unsigned int begin_size = mesh_nodes.size();
+  mesh_nodes.resize(number_of_nodes);
+
+  // create new Node objects and put them in the mesh_nodes list
+  for (unsigned int i=begin_size; i<number_of_nodes; i++) mesh_nodes[i] = new Node();
+  return;
+}
+
+void Mesh::set_offsets(double x_off, double y_off, double z_off){
+  x_offset = x_off;
+  y_offset = y_off;
+  z_offset = z_off;
+  return;
+}
+
+double Mesh::get_offset_x(){
+  return x_offset;
+}
+
+double Mesh::get_offset_y(){
+  return y_offset;
+}
+
+double Mesh::get_offset_z(){
+  return z_offset;
+}
+
+Node * Mesh::get_node_ptr(unsigned int i){
+  return mesh_nodes[i];
+}
+
+Mesh * Mesh::create_regular_grid(double res, unsigned int num_nodes_x, unsigned int num_nodes_y, 
+                      unsigned int num_nodes_z){
+  // declare vars
+  Mesh * mesh_out;
+  Node * node_spawn;
+  unsigned int nodes_total;
+  unsigned int glob_idx;
+
+  // do some input checking
+
+  // fill in the extra member data
+  mesh_out = new Mesh();
+  mesh_out->set_mesh_type(REGULAR);
+  nodes_total = num_nodes_x*num_nodes_y*num_nodes_z;
+  mesh_out->set_num_nodes(nodes_total);
+
+  // begin creating nodes with x, y, z points
+  if (num_nodes_y == 1 && num_nodes_z==1){ // x points only
+    mesh_out->set_num_dims(1);
+    for (unsigned int i=0; i<nodes_total; i++){
+      glob_idx = i;
+      node_spawn = mesh_out->get_node_ptr(glob_idx);
+      node_spawn->x = double(i)*res;
+      node_spawn->boundary = false;
+      
+    }
+    node_spawn = mesh_out->get_node_ptr(0);
+    node_spawn->boundary = true;
+
+    node_spawn = mesh_out->get_node_ptr(mesh_out->get_num_nodes()-1);
+    node_spawn->boundary = true;
+  }
+  else if (num_nodes_z == 1){ // x and y points only
+    mesh_out->set_num_dims(2);
+    for (unsigned int i=0; i<num_nodes_x; i++){
+      for (unsigned int j=0; j<num_nodes_y; j++){
+
+        glob_idx = i*(num_nodes_y) + j;
+        node_spawn = mesh_out->get_node_ptr(glob_idx);
+        node_spawn->x = double(i)*res;
+        node_spawn->y = double(j)*res;
+
+        // boundaries
+        if (i == 0 || j == 0 || i == num_nodes_x-1 || j == num_nodes_y-1) node_spawn->boundary = true;
+        else node_spawn->boundary = false;
+      }
+    }
+  }
+  else{ // x, y, and z points
+    mesh_out->set_num_dims(3);
+    for (unsigned int i=0; i<num_nodes_x; i++){
+      for (unsigned int j=0; j<num_nodes_y; j++){
+        for (unsigned int k=0; k<num_nodes_z; k++){
+
+          glob_idx = i*(num_nodes_y*num_nodes_z) + j*num_nodes_z + k;
+          node_spawn = mesh_out->get_node_ptr(glob_idx);
+          node_spawn->x = double(i)*res;
+          node_spawn->y = double(j)*res;
+          node_spawn->z = double(k)*res;
+
+          // boundaries
+          if (i == 0 || j == 0 || k == 0 || i == num_nodes_x-1 || j == num_nodes_y-1 || k == num_nodes_z-1) node_spawn->boundary = true;
+          else node_spawn->boundary = false;
+        }
+      }
+    }
+  }
+
+  
+
+}
+
+//*************************************************************************************************************
+
+#ifdef _TEST_
+
+int main(int argc, char * argv[]){
+
+  Mesh * mesh_reg_1d = Mesh::create_regular_grid(0.1, 100);
+  mesh_reg_1d->print_summary();
+
+  delete mesh_reg_1d;
+}
+
+#endif
+
--- mesh_class.hpp
+++ mesh_class.hpp
@@ -1,22 +1,36 @@
 #ifndef _MESH_CLASS_H
 #define _MESH_CLASS_H
 
-#include <stdlib>
+#include <stdlib.h>
+
+#include <iostream>
 #include <vector>
 #include <list>
 
-#include "node_class.hpp"
-
 enum MeshType{REGULAR=0, UNSTRUCTURED_TRI=1, UNSTRUCTURED_QUAD=2};
 
-struct Node{
-  unsigned int index; 		// index of point within x,y,z data
-  unsigned int num_neighbors;	// number of neighbors
+class Node{
+public:
+
+  Node();
+  ~Node();
+
+  double x, y, z; 
+  bool boundary;
+  //unsigned int index; 		// index of point within x,y,z data
+  //unsigned int num_neighbors;	// number of neighbors
   std::vector<unsigned int> neighbor_index;		// index of neighbor point within x,y,z data
+
+  unsigned int core_group;
 
   // physical properties
   double epsilon; // relative dielectric constant
-  double mu; // relative permissivity constant
+  double mu; // relative permittivity constant
+
+protected:
+
+private:
+
 };
 
 // cell class for finite volume methods
@@ -37,21 +51,41 @@ public:
   Mesh();		// constructor
   ~Mesh();		// destructor
 
-  create_regular_grid(unsigned int num_nodes_x, unsigned int num_nodes_y, 
-                      unsigned int num_nodes_z); // create a regular grid of points and store it in the mesh
+  void print_summary();
 
+  // member data accessors
+  MeshType get_mesh_type();
+  void set_mesh_type(MeshType type);
+  unsigned int get_num_dims();
+  void set_num_dims(unsigned int ndims);
+  unsigned int get_num_nodes();
+  void set_num_nodes(unsigned int number_of_nodes);
+  void set_offsets(double x_off, double y_off = 0, double z_off = 0);
+  double get_offset_x();
+  double get_offset_y();
+  double get_offset_z();
+
+  Node * get_node_ptr(unsigned int i);
+
+
+  static Mesh* create_regular_grid(double res, unsigned int num_nodes_x, unsigned int num_nodes_y = 1, 
+                      unsigned int num_nodes_z = 1); // create a regular grid of points and store it in the mesh
+  static Mesh* create_unstructured_tri_simple();
 
 protected:
 
 private:
   MeshType mesh_type;
-  unsigned int num_nodes; // number of nodes in the mesh
-  double *x, *y, *z; // the coordinates of each node (z optional)
-  bool *boundary; // flag for boundary points
+  unsigned int num_dims;
+  //unsigned int num_nodes; // number of nodes in the mesh
+  //double *x, *y, *z; // the coordinates of each node (y and z optional)
+  double x_offset, y_offset, z_offset;
+  double xmin, xmax, ymin, ymax, zmin, zmax;
+  //bool *boundary; // flag for boundary points
 
-  unsigned int *core_group; // which core does this node belong to (for parallel processing)
+  //unsigned int * core_group; // which core does this node belong to (for parallel processing)
 
-  std::list<Node> mesh_nodes; // contains Node structures
+  std::vector<Node *> mesh_nodes; // contains pointers to Node structures as a list (so that the mesh is refinable)
 };
 
 
