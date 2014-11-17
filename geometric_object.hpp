8abbe51a59948e8eb48a434f46b7f94ab70f8924
dmp942 <dpederson@utexas.edu>
2014-11-17 11:49:03 -0600

    added a placeholder file for geometric primitive objects (sphere, etc)

---
+++ geometric_object.hpp
@@ -0,0 +1,54 @@
+#ifndef GEOMETRIC_OBJECT_H
+#define GEOMETRIC_OBJECT_H
+
+#include <iostream>
+
+#include <stdlib.h>
+#include <math.h>
+
+
+class geometric_object{
+public:
+
+private:
+
+}
+
+
+class cylinder: public geometric_object{
+public:
+
+private:
+
+}
+
+class sphere: public geometric_object{
+public:
+
+private:
+}
+
+class box: public geometric_object{
+public:
+
+private:
+
+}
+
+class cone: public geometric_object{
+public:
+
+private:
+}
+
+class pyramid: public geometric_object{
+public:
+
+private:
+}
+
+class torus: public geometric_object{
+public:
+
+private:
+}
