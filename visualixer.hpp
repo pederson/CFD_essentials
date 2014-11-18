ce8c82ba97958d64ffd3c05f0d0a46aafca2f2e7
pederson <dpederson@utexas.edu>
2014-11-18 00:15:16 -0600

    added some ideas to visualixer

--- visualixer.hpp
+++ visualixer.hpp
@@ -8,4 +8,37 @@
 //  - visualize the geometry in 2d and 3d and move around in it
 //  - visualize and interact with point clouds
 
+
+// here is the main visualizer class
+class visualixer{
+public:
+
+protected:
+	bool lock_rotation; // lock mouse rotations?
+	float * color_ramp;
+
+};
+
+//*********** here are derived classes ****************
+
+// viewing point clouds
+class cloud_visualixer{
+
+};
+
+// viewing a mesh
+class mesh_visualixer{
+
+};
+
+// viewing a simulation
+class sim_visualixer{
+
+};
+
+// viewing an arbitrary geometry
+class geometry_visualixer{
+
+};
+
 #endif
