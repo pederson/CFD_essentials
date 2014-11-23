6f55a4d61a5502b93f6b5218997eaa3887dfd767
pederson <dpederson@utexas.edu>
2014-11-22 18:31:11 -0600

    tried adding some stuff for glfw

--- visualixer.cpp
+++ visualixer.cpp
@@ -372,6 +372,26 @@ int main(int argc, char * argv[]){
 	// declare vars
 
 	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	cout << "yup" << endl;
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL); // Windowed
+	//GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", glfwGetPrimaryMonitor(), nullptr); // Fullscreen
+
+	glfwMakeContextCurrent(window);
+	cout << "made context current" << endl;
+	while(!glfwWindowShouldClose(window)){
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+		//if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    	//glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+
 	//std::this_thread::sleep_for(std::chrono::seconds(1));
 	glfwTerminate();
 	
