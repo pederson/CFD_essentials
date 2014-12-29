abf2462312dde8afaeb397fd3a6bb6b3eabd807c
pederson <dpederson@utexas.edu>
2014-12-29 15:40:37 -0600

    made some organization changes and added manual issue tracking and task list

--- mesh_data.hpp
+++
@@ -1,6 +0,0 @@
-// this will hold a data class over a mesh.
-// it is intended to store data from simulations into class.
-// This will allow the user to push the data on to a visualizer
-//   for later viewing, or allow the user to take snapshots of 
-//   variables that may be changing over the mesh in time (dielectric
-//   constant in a plasma, for example)
