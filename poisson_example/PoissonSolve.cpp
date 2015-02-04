644462ed8988f78d8e25707543546e33af9be59c
pederson <dpederson@utexas.edu>
2015-02-03 18:36:46 -0600

    got closer to solving the poisson problem

--- poisson_example/PoissonSolve.cpp
+++ poisson_example/PoissonSolve.cpp
@@ -16,7 +16,7 @@ int main(int argc, char * argv[]){
 	my_param2.add_material("Air", {1.0});
 	my_param2.add_material("Dielectric", {5.0});
 	my_param2.add_material("Dielectric2", {9.0});
-	gaussian_2d ga1 = gaussian_2d(0.3, 0.3, 10.0, 1.0, {0.0, 0.0});
+	gaussian_2d ga1 = gaussian_2d(0.3, 0.3, 100.0, 1.0e18, {0.0, 0.0}); // electron density in #/m^3
 	my_param2.add_object(&ga1);
 
 	// convert the model into a mesh
@@ -35,11 +35,55 @@ int main(int argc, char * argv[]){
 	// this is a testing section
 		// solve the poisson equation using a finite difference method
 	//Simulation mysim = Simulation(FINITE_DIFFERENCE, paramesh);
-	//mysim.
+	
+
+	// loop over the internal nodes adding the laplace operator to the matrix
+	double ** mat = new double *[paramesh->num_nodes_y()*paramesh->num_nodes_x()];
+	for (auto i=0; i<paramesh->num_nodes_y()*paramesh->num_nodes_x(); i++) mat[i] = new double[paramesh->num_nodes_y()*paramesh->num_nodes_x()];
+	for (auto i=0; i<paramesh->num_nodes_x(); i++){ // columns
+		for (auto j=0; j<paramesh->num_nodes_y(); j++) mat[i][j] = 0;
+	}
+
+	unsigned int cind, lind, rind, uind, dind;
+	for (auto i=1; i<paramesh->num_nodes_x()-1; i++){ // columns
+		for (auto j=1; j<paramesh->num_nodes_y()-1; j++){ // rows
+			cind = paramesh->reg_inds_to_glob_ind(i,j);
+			lind = paramesh->reg_inds_to_glob_ind(i-1,j);
+			rind = paramesh->reg_inds_to_glob_ind(i+1,j);
+			uind = paramesh->reg_inds_to_glob_ind(i,j+1);
+			dind = paramesh->reg_inds_to_glob_ind(i,j-1);
+			mat[cind][cind] = -4;
+			mat[cind][lind] = 1;
+			mat[cind][rind] = 1;
+			mat[cind][uind] = 1;
+			mat[cind][dind] = 1;
+		}
+	}
+
+	// write in the boundary conditions
+	// zeros for now...just testing the code
+
+	// construct the right side using the density field
+	double q_electron = -1.6e-19, eps0 = 8.854e-12;
+	const double * reldata = &paramesh->data("e_density");
+	double * rhs = new double[paramesh->num_nodes_y()*paramesh->num_nodes_x()];
+	for (auto i=1; i<paramesh->num_nodes_x()-1; i++){ // columns
+		for (auto j=1; j<paramesh->num_nodes_y()-1; j++){ // rows
+			cind = paramesh->reg_inds_to_glob_ind(i,j);
+			rhs[cind] = -reldata[cind]*q_electron/eps0;
+		}
+	}
+
+	// now solve the system (PETSc)
+
+	// visualize the solution by putting it in the mesh
+
+
+
 
 	/* this is what I would do if I had all the classes ready
 	// create an equation
-	// -del^2(potential) = e*(e_density)/epsilon
+	// del^2(potential) = -e*(e_density)/epsilon
 	//Equation poiss = Equation(EQUATION_POISSON);
 
 	// set boundary conditions
